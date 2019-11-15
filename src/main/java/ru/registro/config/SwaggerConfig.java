d54c86f69fdc88fe72fffabf2c664ce7f95521f1
tip <aik442557@gmail.com>
2019-11-15 22:38:08 +0300

    fix Swagger

--- src/main/java/ru/registro/config/SwaggerConfig.java
+++ src/main/java/ru/registro/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("ru.krtech.dialogm.bot.controller"))
+                .apis(RequestHandlerSelectors.basePackage("ru.registro.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
